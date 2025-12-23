89b65fb8d2eaa1da6bee2209a2f4e942268a7d50
ajaylohar <loharajay0019@gmail.com>
2025-12-23 10:09:54 +0530

    mongodb configuration

---
+++ foodiesapi/src/test/java/in/ajay/foodiesapi/FoodiesapiApplicationTests.java
@@ -0,0 +1,13 @@
+package in.ajay.foodiesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodiesapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
