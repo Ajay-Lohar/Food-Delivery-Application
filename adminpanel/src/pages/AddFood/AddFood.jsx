0e992c689d1f5ff8e60646baf2a5a0faed3e2974
ajaylohar <loharajay0019@gmail.com>
2025-12-27 12:08:23 +0530

    create api add food

--- adminpanel/src/pages/AddFood/AddFood.jsx
+++ adminpanel/src/pages/AddFood/AddFood.jsx
@@ -1,33 +1,72 @@
-import React, { useState } from 'react';
-import {assets} from '../../assets/assets'
+import React, {  useState } from 'react';
+import {assets} from '../../assets/assets';
+import axios from 'axios';
 
 const AddFood = () => {
-  const [image, setImage] =useState(false);
+  const [image, setImage] = useState(false);
+  const [data, setData]   = useState({
+    name:'',
+    description:'',
+    price:'',
+    category:'Biryani'
+  });
+
+   const onChangeHandler = (event)=>{
+    const name =event.target.name;
+    const value=event.target.value;
+    setData (data=>({...data ,[name]: value}));
+   }
+
+   const onSubmitHandler= async(event)=>{
+      event.preventDefault();
+      if(!image){
+        alert('please select an image.');
+        return;
+      }
+
+      const formData = new FormData();
+      formData.append('food', JSON.stringify(data));
+      formData.append('file',image);
+
+      try{
+       const response = await axios.post('http://localhost:8080/api/foods',formData, {headers:{"Content-Type": "multipart/form-data"}});
+       if(response.status ===200){
+        alert ('food added successfuly.');
+        setData({name:'',description:'',category:'Biryani',price:''});
+        setImage(null);
+       }
+      }catch(error){
+        console.log('Error',error);
+        alert('Error adding Food');
+         
+      }
+   }
+  
   return (
     <div className="mx-2 mt-2">
   <div className="row ">
     <div className="card col-md-4">
       <div className="card-body ">
         <h2 className="mb-3">Add Food</h2>
-        <form>
+        <form onSubmit={onSubmitHandler}>
           <div className="mb-3">
             <label htmlFor="image" className="form-label">
               <img src={image? URL.createObjectURL(image):assets.upload} alt="" width={90} />
             </label>
-            <input type="file" className="form-control" id="image"  required hidden onChange={(e) =>setImage(e.target.files[0])} />
+            <input type="file" className="form-control" id="image"   hidden onChange={(e) =>setImage(e.target.files[0])} />
           </div>
           <div className="mb-3">
             <label htmlFor="name" className="form-label">Name</label>
-            <input type="text" className="form-control" id="name" name='name' required />
+            <input type="text" className="form-control" id="name" name='name' required onChange={onChangeHandler} value={data.name}/>
           </div>
           
           <div className="mb-3">
             <label htmlFor="description" className="form-label">Description</label>
-            <textarea type="text" className="form-control" id="description" name='description' required />
+            <textarea type="text" className="form-control" id="description" name='description' required onChange={onChangeHandler} value={data.description}/>
           </div>
           <div className="mb-3">
             <label htmlFor="categoty" className="form-label">Category</label>
-            <select name="categoty" id="categoty" className='form-control'>
+            <select name="categoty" id="categoty" className='form-control' onChange={onChangeHandler} value={data.categoty}>
               <option value="Biryani">Biryani</option>
               <option value="Cake">Cake</option>
               <option value="Pizza">Pizza</option>
@@ -39,7 +78,7 @@ const AddFood = () => {
           </div>
           <div className="mb-3">
             <label htmlFor="price" className="form-label">Price</label>
-            <input type="number" name='price' id='price' className='form-control' />
+            <input type="number" name='price' id='price' className='form-control'onChange={onChangeHandler} value={data.price} />
           </div>
           <button type="submit" className="btn btn-primary">Save</button>
         </form>
