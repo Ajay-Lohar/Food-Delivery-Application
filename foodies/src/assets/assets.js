08124b2ee713a2030ee2df473617dc81b12b4c76
ajaylohar <loharajay0019@gmail.com>
2025-12-27 23:10:48 +0530

    .

---
+++ foodies/src/assets/assets.js
@@ -0,0 +1,48 @@
+import logo from './logo.png';
+import cart from './cart.webp';
+import biryani from './biryani.webp';
+import burger from './burger.webp';
+import cake from './cake.webp';
+import icecream from './icecream.webp';
+import pizza from './pizza.webp';
+import salad from './salad.webp';
+import rolls from './rolls.webp';
+
+
+export const assets ={
+    logo, 
+    cart
+   
+}
+
+export const categories =[
+
+   { 
+    category:'Biryani',
+    icon:biryani
+   },
+   { 
+    category:'Burger',
+    icon:burger
+   },
+   { 
+    category:'Cake',
+    icon:cake
+   },
+   { 
+    category:'Ice Creams',
+    icon:icecream
+   },
+   { 
+    category:'Pizza',
+    icon:pizza
+   },
+   { 
+    category:'Salad',
+    icon:salad
+   },
+   { 
+    category:'Rolls',
+    icon:rolls
+   }
+]
