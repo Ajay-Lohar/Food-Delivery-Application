a373ff07bc5cae48c34ee6806ef7094bf1c697af
ajaylohar <loharajay0019@gmail.com>
2025-12-29 22:46:42 +0530

    add increase Qty btn

--- foodies/src/context/StoreContext.jsx
+++ foodies/src/context/StoreContext.jsx
@@ -5,11 +5,25 @@ import { fetchFoodList } from "../service/foodService";
 export const StoreContext = createContext(null);
 
 export const StoreContextProvider =(props) =>{
-    const [foodList, setFoodList] = useState([])
+    const [foodList, setFoodList] = useState([]);
 
- 
+    const [quantities,setQuantities]=useState({});
+
+    const increaseQty = (foodId) =>{
+        setQuantities((prev)=>({...prev, [foodId] : (prev[foodId] || 0) + 1}));
+
+    }
+
+    const decreaseQty = (foodId) =>{
+        setQuantities((prev) =>({...prev,[foodId]: prev[foodId]> 0 ? prev[foodId] -1 : 0}))
+
+    }
+
     const contextValue = {
-        foodList
+        foodList,
+        increaseQty,
+        decreaseQty,
+        quantities
     };
     useEffect(()=>{
        async  function loadData() {
