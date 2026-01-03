2e44a1d2088e8e131498cf130e70f42ac28ad1a4
ajaylohar <loharajay0019@gmail.com>
2026-01-03 18:01:34 +0530

    Jwt Token complete login and register api's

--- foodiesapi/src/main/java/in/ajay/foodiesapi/config/SecurityConfig.java
+++ foodiesapi/src/main/java/in/ajay/foodiesapi/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package in.ajay.foodiesapi.config;
 
+import in.ajay.foodiesapi.filters.JwtAuthenticationFilter;
 import in.ajay.foodiesapi.service.AppUserDeatailsService;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -15,6 +16,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
@@ -27,6 +29,7 @@ import java.util.List;
 public class SecurityConfig {
 
     private  final AppUserDeatailsService UserDeatailsService;
+    private  final JwtAuthenticationFilter jwtAuthenticationFilter;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
@@ -34,8 +37,10 @@ public class SecurityConfig {
                 .cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth.requestMatchers("/api/register","/api/login","/api/foods/**").permitAll().anyRequest().authenticated() )
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-            return http.build();
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
     }
 
     @Bean
