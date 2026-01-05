3785d466d9b345f23340c2f44fbc9c3abbb96547
ajaylohar <loharajay0019@gmail.com>
2026-01-05 21:57:42 +0530

    .

--- foodiesapi/src/main/java/in/ajay/foodiesapi/io/OrderItem.java
+++ foodiesapi/src/main/java/in/ajay/foodiesapi/io/OrderItem.java
@@ -1,10 +1,13 @@
 package in.ajay.foodiesapi.io;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
-@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderItem {
 
     private String foodId;
